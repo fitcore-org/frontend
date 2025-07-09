8a2434f7690f37a44d8421e7f2bbb4097962c9df
Lucas Vinicius Marques Barbosa <lvmb@WINDOWS.CIN.UFPE.BR>
2025-07-09 20:14:03 -0300

    switch button

--- FitCore/src/app/components/main/main.ts
+++ FitCore/src/app/components/main/main.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.html',
-  styleUrl: './main.scss'
+  styleUrls: ['./main.scss']
 })
 export class Main {
+  tab = 'alunos';
 
+  getSliderPosition() {
+    switch (this.tab) {
+      case 'alunos':
+        return 'translateX(0%)';
+      case 'colaboradores':
+        return 'translateX(100%)';
+      case 'financeiro':
+        return 'translateX(200%)';
+      default:
+        return 'translateX(0%)';
+    }
+  }
 }
